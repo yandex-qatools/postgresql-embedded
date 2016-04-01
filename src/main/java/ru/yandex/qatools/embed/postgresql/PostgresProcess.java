20f5dc6d29cfa7e56e0d11dbd46ec916d2f8392b
Eduard Vrastil <eduard.vrastil@hp.com>
2016-04-01 13:21:16 +0200

    use pg_ctl wrapper instead of postgres - add forgotten host param

--- src/main/java/ru/yandex/qatools/embed/postgresql/PostgresProcess.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/PostgresProcess.java
@@ -151,7 +151,7 @@ public class PostgresProcess extends AbstractPGProcess<PostgresExecutable, Postg
         ret.addAll(asList(exe.executable().getAbsolutePath(),
                 "start",
                 "-o",
-                String.format("\"-p %s\"", String.valueOf(config.net().port())),
+                String.format("\"-p %s -h %s\"", config.net().port(), config.net().host()),
                 "-D", config.storage().dbDir().getAbsolutePath(),
                 "-w"
         ));
