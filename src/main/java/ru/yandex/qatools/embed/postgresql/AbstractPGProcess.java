fd6088bad21fb8d3bdf86b1f417f814004e47891
Ilya Sadykov <smecsia@gmail.com>
2016-05-03 12:45:12 +0300

    Add jre/bin to PATH for every postgres process (refs #21) Add log4j for tests

--- src/main/java/ru/yandex/qatools/embed/postgresql/AbstractPGProcess.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/AbstractPGProcess.java
@@ -9,6 +9,9 @@ import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
 
 import java.io.IOException;
 
+import static java.io.File.*;
+import static org.apache.commons.lang3.SystemUtils.JAVA_HOME;
+
 public abstract class AbstractPGProcess<E extends Executable<PostgresConfig, P>, P extends IStopable>
         extends AbstractProcess<PostgresConfig, E, P> {
 
@@ -22,6 +25,12 @@ public abstract class AbstractPGProcess<E extends Executable<PostgresConfig, P>,
             processBuilder.environment().put("PGUSER", config.credentials().username());
             processBuilder.environment().put("PGPASSWORD", config.credentials().password());
         }
+        processBuilder.environment().put("PATH",
+                processBuilder.environment().get("PATH") + pathSeparatorChar
+                        + JAVA_HOME + separator + "bin"
+                        + pathSeparatorChar
+                        + JAVA_HOME + separator + "jre" + separator + "bin"
+        );
     }
 
     @Override
