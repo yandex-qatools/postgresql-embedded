a8e9c455796e51566c5fdcd4064056b0def6f042
Sumit Shah <shah@ucla.edu>
2016-09-03 12:33:43 -0700

    use logging listener

--- src/main/java/ru/yandex/qatools/embed/postgresql/config/DownloadConfigBuilder.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/config/DownloadConfigBuilder.java
@@ -3,9 +3,12 @@ package ru.yandex.qatools.embed.postgresql.config;
 import de.flapdoodle.embed.process.config.store.DownloadPath;
 import de.flapdoodle.embed.process.extract.UUIDTempNaming;
 import de.flapdoodle.embed.process.io.directories.UserHome;
-import de.flapdoodle.embed.process.io.progress.StandardConsoleProgressListener;
+import de.flapdoodle.embed.process.io.progress.LoggingProgressListener;
 import ru.yandex.qatools.embed.postgresql.Command;
 import ru.yandex.qatools.embed.postgresql.PackagePaths;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 /**
@@ -21,16 +24,7 @@ public class DownloadConfigBuilder extends de.flapdoodle.embed.process.config.st
         artifactStorePath().setDefault(new UserHome(".embedpostgresql"));
         downloadPrefix().setDefault(new DownloadPrefix("posgresql-download"));
         userAgent().setDefault(new UserAgent("Mozilla/5.0 (compatible; Embedded postgres; +https://github.com/yandex-qatools)"));
-        progressListener().setDefault(new StandardConsoleProgressListener() {
-            @Override
-            public void info(String label, String message) {
-                if(label.startsWith("Extract")){
-                    System.out.print(".");//NOSONAR
-                } else {
-                    super.info(label, message);//NOSONAR
-                }
-            }
-        });
+        progressListener().setDefault(new LoggingProgressListener(Logger.getLogger(getClass().getName()), Level.INFO));
         return this;
     }
 
