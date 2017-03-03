a035c2efeb1c4d6bbdc279ee7288361dbb1c55a8
Ilya Sadykov <smecsia@gmail.com>
2017-03-03 13:55:10 +1100

    Provide cached artifact store implementation (fixes #65)

--- src/main/java/ru/yandex/qatools/embed/postgresql/config/DownloadConfigBuilder.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/config/DownloadConfigBuilder.java
@@ -6,6 +6,7 @@ import de.flapdoodle.embed.process.io.directories.UserHome;
 import de.flapdoodle.embed.process.io.progress.StandardConsoleProgressListener;
 import ru.yandex.qatools.embed.postgresql.Command;
 import ru.yandex.qatools.embed.postgresql.PackagePaths;
+import ru.yandex.qatools.embed.postgresql.ext.SubdirTempDir;
 
 
 /**
@@ -17,7 +18,7 @@ public class DownloadConfigBuilder extends de.flapdoodle.embed.process.config.st
         fileNaming().setDefault(new UUIDTempNaming());
         // I've found the only open and easy to use cross platform binaries
         downloadPath().setDefault(new DownloadPath("http://get.enterprisedb.com/postgresql/"));
-        packageResolver().setDefault(new PackagePaths(command));
+        packageResolver().setDefault(new PackagePaths(command, SubdirTempDir.defaultInstance()));
         artifactStorePath().setDefault(new UserHome(".embedpostgresql"));
         downloadPrefix().setDefault(new DownloadPrefix("posgresql-download"));
         userAgent().setDefault(new UserAgent("Mozilla/5.0 (compatible; Embedded postgres; +https://github.com/yandex-qatools)"));
---
+++ src/test/java/ru/yandex/qatools/embed/postgresql/TestPostgresCachedDirStarter.java
@@ -0,0 +1,28 @@
+package ru.yandex.qatools.embed.postgresql;
+
+import de.flapdoodle.embed.process.config.IRuntimeConfig;
+import de.flapdoodle.embed.process.io.directories.FixedPath;
+import ru.yandex.qatools.embed.postgresql.config.DownloadConfigBuilder;
+import ru.yandex.qatools.embed.postgresql.config.RuntimeConfigBuilder;
+import ru.yandex.qatools.embed.postgresql.ext.CachedArtifactStoreBuilder;
+
+public class TestPostgresCachedDirStarter extends TestPostgresStarter {
+
+    @Override
+    protected IRuntimeConfig buildRuntimeConfig() {
+        // turns off the default functionality of unzipping on every run.
+        final String tmpDir = "/tmp/pgembed";
+        final Command cmd = Command.Postgres;
+        final FixedPath cachedDir = new FixedPath(tmpDir);
+        return new RuntimeConfigBuilder()
+                .defaults(cmd)
+                .artifactStore(new CachedArtifactStoreBuilder()
+                        .defaults(cmd)
+                        .tempDir(cachedDir)
+                        .download(new DownloadConfigBuilder()
+                                .defaultsForCommand(cmd)
+                                .packageResolver(new PackagePaths(cmd, cachedDir))
+                                .build()))
+                .build();
+    }
+}
