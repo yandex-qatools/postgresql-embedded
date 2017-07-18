46952972cac41ef3db69fe21ee6534723b3f2681
Victor Alenkov (victor@alenkov.name) <victor@alenkov.name>
2017-07-18 18:06:57 +0300

    issue #41: 0) change tests - using hamcrest matchers

--- src/test/java/ru/yandex/qatools/embed/postgresql/TestMultipleInstance.java
+++ src/test/java/ru/yandex/qatools/embed/postgresql/TestMultipleInstance.java
@@ -1,7 +1,5 @@
 package ru.yandex.qatools.embed.postgresql;
 
-import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
 import ru.yandex.qatools.embed.postgresql.distribution.Version;
 
@@ -9,18 +7,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.junit.Assert.assertThat;
+
 public class TestMultipleInstance {
 	@Test
 	public void itShouldAllowToRunTwoInstancesWithDifferentVersions() throws Exception {
 		final EmbeddedPostgres postgres0 = new EmbeddedPostgres();
 		postgres0.start();
-		Assert.assertTrue(postgres0.getConnectionUrl().isPresent());
+		assertThat(postgres0.getConnectionUrl().isPresent(), is(true));
 		checkVersion(postgres0.getConnectionUrl().get(), "PostgreSQL 9.6");
 		postgres0.stop();
 
 		final EmbeddedPostgres postgres1 = new EmbeddedPostgres(Version.Main.V9_5);
 		postgres1.start();
-		Assert.assertTrue(postgres1.getConnectionUrl().isPresent());
+		assertThat(postgres1.getConnectionUrl().isPresent(), is(true));
 		checkVersion(postgres1.getConnectionUrl().get(), "PostgreSQL 9.5");
 		postgres1.stop();
 	}
@@ -29,12 +31,13 @@ public class TestMultipleInstance {
 	public void itShouldAllowToRunTwoInstancesAtSameTime() throws Exception {
 		final EmbeddedPostgres postgres0 = new EmbeddedPostgres();
 		postgres0.start();
-		Assert.assertTrue(postgres0.getConnectionUrl().isPresent());
-		checkVersion(postgres0.getConnectionUrl().get(), "PostgreSQL 9.6");
+		assertThat(postgres0.getConnectionUrl().isPresent(), is(true));
 
 		final EmbeddedPostgres postgres1 = new EmbeddedPostgres();
 		postgres1.start();
-		Assert.assertTrue(postgres1.getConnectionUrl().isPresent());
+		assertThat(postgres1.getConnectionUrl().isPresent(), is(true));
+
+		checkVersion(postgres0.getConnectionUrl().get(), "PostgreSQL 9.6");
 		checkVersion(postgres1.getConnectionUrl().get(), "PostgreSQL 9.6");
 
 		postgres0.stop();
@@ -45,11 +48,11 @@ public class TestMultipleInstance {
 	public void itShouldAllowToRunTwoInstancesAtSameTimeAndWithDifferentVersions() throws Exception {
 		final EmbeddedPostgres postgres0 = new EmbeddedPostgres(Version.Main.V9_5);
 		postgres0.start();
-		Assert.assertTrue(postgres0.getConnectionUrl().isPresent());
+		assertThat(postgres0.getConnectionUrl().isPresent(), is(true));
 
 		final EmbeddedPostgres postgres1 = new EmbeddedPostgres(Version.Main.V9_6);
 		postgres1.start();
-		Assert.assertTrue(postgres1.getConnectionUrl().isPresent());
+		assertThat(postgres1.getConnectionUrl().isPresent(), is(true));
 
 		checkVersion(postgres0.getConnectionUrl().get(), "PostgreSQL 9.5");
 		checkVersion(postgres1.getConnectionUrl().get(), "PostgreSQL 9.6");
@@ -61,9 +64,9 @@ public class TestMultipleInstance {
 	private void checkVersion(String jdbcUrl, String expectedVersion) throws Exception {
 		try (final Connection conn = DriverManager.getConnection(jdbcUrl);
 		     final Statement statement = conn.createStatement()) {
-			Assert.assertTrue(statement.execute("SELECT version();"));
-			Assert.assertTrue(statement.getResultSet().next());
-			Assert.assertTrue(statement.getResultSet().getString("version").contains(expectedVersion));
+			assertThat(statement.execute("SELECT version();"), is(true));
+			assertThat(statement.getResultSet().next(), is(true));
+			assertThat(statement.getResultSet().getString("version"), containsString(expectedVersion));
 		}
 	}
 }
--- src/test/java/ru/yandex/qatools/embed/postgresql/config/StorageTest.java
+++ src/test/java/ru/yandex/qatools/embed/postgresql/config/StorageTest.java
@@ -4,6 +4,9 @@ import org.junit.Assert;
 import org.junit.Test;
 import ru.yandex.qatools.embed.postgresql.EmbeddedPostgres;
 
+import static org.hamcrest.core.IsNot.not;
+import static org.junit.Assert.assertThat;
+
 public class StorageTest {
 
 	@Test
@@ -14,6 +17,6 @@ public class StorageTest {
 		final AbstractPostgresConfig.Storage storage1 = new AbstractPostgresConfig.Storage(EmbeddedPostgres.DEFAULT_DB_NAME);
 		Assert.assertTrue(storage1.dbDir().exists());
 
-		Assert.assertNotEquals(storage0.dbDir().getPath(), storage1.dbDir().getPath());
+		assertThat(storage0.dbDir().getPath(), not(storage1.dbDir().getPath()));
 	}
 }
