7bda36072afcf3f42bb09d0ecf193ad7535cf498
Ilya Sadykov <smecsia@gmail.com>
2017-07-26 17:54:56 +1000

    Fix temp dir

--- .appveyor-run-mvn.bat
+++ .appveyor-run-mvn.bat
@@ -1,5 +1,7 @@
 SET JAVA_HOME=C:\Program Files\Java\jdk1.8.0
 SET M3_HOME=C:\maven\apache-maven-3.2.5
+SET TEMP=C:\Users\postgres\AppData\Local\Temp
+SET TMP=C:\Users\postgres\AppData\Local\Temp
 SET M2_HOME=
 SET MAVEN_OPTS=-XX:MaxPermSize=2g -Xmx4g
 SET JAVA_OPTS=-XX:MaxPermSize=2g -Xmx4g
