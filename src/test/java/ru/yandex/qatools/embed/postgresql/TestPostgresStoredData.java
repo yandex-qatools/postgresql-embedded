056df7d344d3a1f52481840a86a4336b2ff4f526
Victor Alenkov (victor@alenkov.name) <victor@alenkov.name>
2017-07-26 11:03:01 +0300

    Removed obsolete tests

--- src/test/java/ru/yandex/qatools/embed/postgresql/TestPostgresStoredData.java
+++ src/test/java/ru/yandex/qatools/embed/postgresql/TestPostgresStoredData.java
@@ -1,29 +1,22 @@
 package ru.yandex.qatools.embed.postgresql;
 
-import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import ru.yandex.qatools.embed.postgresql.config.AbstractPostgresConfig;
-import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
 import ru.yandex.qatools.embed.postgresql.distribution.Version;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static java.lang.String.format;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
-import static ru.yandex.qatools.embed.postgresql.EmbeddedPostgres.*;
-import static ru.yandex.qatools.embed.postgresql.util.SocketUtil.findFreePort;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TestPostgresStoredData {
@@ -33,36 +26,6 @@ public class TestPostgresStoredData {
 	public static void setUpClass() throws Exception {
 		baseDir = Files.createTempDirectory("data").toFile();
 		baseDir.deleteOnExit();
-	}
-
-	@Test
-	public void testStep0() throws Exception {
-		final PostgresProcess process = buildProcess(baseDir);
-		Assert.assertTrue(process.getConfig().storage().dbDir().exists());
-		Connection connection = DriverManager.getConnection(formatConnUrl(process.getConfig()));
-		initSchema(connection);
-		checkRecords(connection);
-		process.stop();
-	}
-
-	@Test
-	public void testStep1() throws Exception {
-		final PostgresProcess process = buildProcess(baseDir);
-		Assert.assertTrue(process.getConfig().storage().dbDir().exists());
-		Connection connection = DriverManager.getConnection(formatConnUrl(process.getConfig()));
-		checkRecords(connection);
-		process.stop();
-	}
-
-	@Test
-	public void testStep2() throws Exception {
-		final EmbeddedPostgres embeddedPostgres = new EmbeddedPostgres(baseDir.getAbsolutePath());
-		final String           jdbcUrl          = embeddedPostgres.start();
-		try (Connection connection = DriverManager.getConnection(jdbcUrl)) {
-			checkRecords(connection);
-		} finally {
-			embeddedPostgres.stop();
-		}
 	}
 
 	@Test
@@ -91,7 +54,18 @@ public class TestPostgresStoredData {
 	}
 
 	@Test
-	public void testStep3() throws Exception {
+	public void testStep0() throws Exception {
+		final EmbeddedPostgres embeddedPostgres = new EmbeddedPostgres(baseDir.getAbsolutePath());
+		final String           jdbcUrl          = embeddedPostgres.start();
+		try (Connection connection = DriverManager.getConnection(jdbcUrl)) {
+			checkRecords(connection);
+		} finally {
+			embeddedPostgres.stop();
+		}
+	}
+
+	@Test
+	public void testStep1() throws Exception {
 		final EmbeddedPostgres embeddedPostgres = new EmbeddedPostgres(Version.Main.PRODUCTION, baseDir.getAbsolutePath());
 		final String           jdbcUrl          = embeddedPostgres.start();
 		try (Connection connection = DriverManager.getConnection(jdbcUrl)) {
@@ -114,25 +88,4 @@ public class TestPostgresStoredData {
 		assertThat(statement.getResultSet().getString("code"), is("movie"));
 	}
 
-	private PostgresProcess buildProcess(File baseDir) throws IOException {
-		final PostgresStarter<PostgresExecutable, PostgresProcess> runtime = PostgresStarter.getInstance(EmbeddedPostgres.defaultRuntimeConfig());
-		PostgresConfig config = new PostgresConfig(Version.Main.PRODUCTION,
-		                                           new AbstractPostgresConfig.Net(DEFAULT_HOST, findFreePort()),
-		                                           new AbstractPostgresConfig.Storage(DEFAULT_DB_NAME, baseDir.getPath()),
-		                                           new AbstractPostgresConfig.Timeout(),
-		                                           new AbstractPostgresConfig.Credentials(DEFAULT_USER, DEFAULT_PASSWORD)
-		);
-		PostgresExecutable exec = runtime.prepare(config);
-		return exec.start();
-	}
-
-	private String formatConnUrl(PostgresConfig config) {
-		return format("jdbc:postgresql://%s:%s/%s?user=%s&password=%s",
-		              config.net().host(),
-		              config.net().port(),
-		              config.storage().dbName(),
-		              config.credentials().username(),
-		              config.credentials().password()
-		             );
-	}
 }
