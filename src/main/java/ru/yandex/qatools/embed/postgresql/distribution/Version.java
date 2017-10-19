29c5738f3cd1c38368e8e5bd117d6afb2373a147
t9t <t9t@users.noreply.github.com>
2017-10-19 12:04:36 +0200

    Update 9.6 version to 9.6.5

--- src/main/java/ru/yandex/qatools/embed/postgresql/distribution/Version.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/distribution/Version.java
@@ -6,7 +6,7 @@ import de.flapdoodle.embed.process.distribution.IVersion;
  * PostgreSQL Version enum
  */
 public enum Version implements IVersion {
-    V9_6_3("9.6.3-1"),
+    V9_6_5("9.6.5-1"),
     @Deprecated V9_5_7("9.5.7-1"),;
 
     private final String specificVersion;
@@ -27,7 +27,7 @@ public enum Version implements IVersion {
 
     public enum Main implements IVersion {
         V9_5(V9_5_7),
-        V9_6(V9_6_3),
+        V9_6(V9_6_5),
         PRODUCTION(V9_6);
 
         private final IVersion _latest;
