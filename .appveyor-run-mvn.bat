372ebdbf3384000f7e8c1c9aad13706d17313587
Ilya <smecsia@list.ru>
2017-10-24 16:54:21 +1100

    AppVeyor configuration to return correct exit code (#103)
    AppVeyor configuration to return correct exit code

--- .appveyor-run-mvn.bat
+++ .appveyor-run-mvn.bat
@@ -6,4 +6,5 @@ SET M2_HOME=
 SET MAVEN_OPTS=-XX:MaxPermSize=2g -Xmx4g
 SET JAVA_OPTS=-XX:MaxPermSize=2g -Xmx4g
 SET PATH=C:\maven\apache-maven-3.2.5\bin;%JAVA_HOME%\bin;C:\Program Files\OpenSSH\bin;%PATH%
-mvn %*
+call mvn %*
+echo %errorlevel% 1> C:\Users\postgres\src\target\exit-code.txt
