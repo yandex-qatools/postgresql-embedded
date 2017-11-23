004468589e93795dd46946e36c2066fdccdf50c5
Ilya <smecsia@list.ru>
2017-11-23 13:10:14 +1100

    More verbose logging to debug failures (refs #110) (#111)

--- src/main/java/ru/yandex/qatools/embed/postgresql/InitDbProcess.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/InitDbProcess.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (C) 2011
- *   Michael Mosmann <michael@mosmann.de>
- *   Martin Jöhren <m.joehren@googlemail.com>
- *
+ * Michael Mosmann <michael@mosmann.de>
+ * Martin Jöhren <m.joehren@googlemail.com>
+ * <p>
  * with contributions from
- * 	konstantin-ba@github, Archimedes Trajano (trajano@github), Christian Bayer (chrbayer84@googlemail.com)
- *
+ * konstantin-ba@github, Archimedes Trajano (trajano@github), Christian Bayer (chrbayer84@googlemail.com)
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -25,22 +25,24 @@ import de.flapdoodle.embed.process.config.io.ProcessOutput;
 import de.flapdoodle.embed.process.distribution.Distribution;
 import de.flapdoodle.embed.process.distribution.Platform;
 import de.flapdoodle.embed.process.extract.IExtractedFileSet;
-import de.flapdoodle.embed.process.io.LogWatchStreamProcessor;
 import de.flapdoodle.embed.process.io.Processors;
 import de.flapdoodle.embed.process.io.StreamToLineProcessor;
 import de.flapdoodle.embed.process.io.file.Files;
 import de.flapdoodle.embed.process.runtime.ProcessControl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.yandex.qatools.embed.postgresql.config.PostgresConfig;
+import ru.yandex.qatools.embed.postgresql.ext.LogWatchStreamProcessor;
 import ru.yandex.qatools.embed.postgresql.ext.SubdirTempDir;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 import static de.flapdoodle.embed.process.io.file.Files.createTempFile;
 import static java.util.Arrays.asList;
+import static java.util.Collections.singleton;
 import static java.util.UUID.randomUUID;
 
 /**
@@ -48,6 +50,7 @@ import static java.util.UUID.randomUUID;
  * (helper to initialize the DB)
  */
 class InitDbProcess<E extends InitDbExecutable> extends AbstractPGProcess<E, InitDbProcess> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(InitDbProcess.class);
 
     public InitDbProcess(Distribution distribution, PostgresConfig config, IRuntimeConfig runtimeConfig, E executable) throws IOException {
         super(distribution, config, runtimeConfig, executable);
@@ -80,10 +83,10 @@ class InitDbProcess<E extends InitDbExecutable> extends AbstractPGProcess<E, Ini
 
     @Override
     protected void onAfterProcessStart(ProcessControl process, IRuntimeConfig runtimeConfig) throws IOException {
-        ProcessOutput outputConfig = runtimeConfig.getProcessOutput();
-        LogWatchStreamProcessor logWatch = new LogWatchStreamProcessor(
-                "database system is ready to accept connections",
-                new HashSet<>(asList("[initdb error]")), StreamToLineProcessor.wrap(outputConfig.getOutput()));
+        final ProcessOutput outputConfig = runtimeConfig.getProcessOutput();
+        final LogWatchStreamProcessor logWatch = new LogWatchStreamProcessor(
+                "performing post-bootstrap initialization",
+                singleton("[initdb error]"), StreamToLineProcessor.wrap(outputConfig.getOutput()));
         Processors.connect(process.getReader(), logWatch);
         Processors.connect(process.getError(), StreamToLineProcessor.wrap(outputConfig.getError()));
         logWatch.waitForResult(getConfig().timeout().startupTimeout());
