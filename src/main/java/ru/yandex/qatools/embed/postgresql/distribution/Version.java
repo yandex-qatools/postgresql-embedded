75c8d18ffa5387d6d00f8fc334973a6893684673
julianladisch <julianladisch@users.noreply.github.com>
2018-02-22 12:39:13 +0100

    Security update 10.2, 9.6.7, 9.5.11 (#120)
    Two security vulnerabilities have been fixed by PostgreSQL 10.2, 9.6.7, 9.5.11:
    
    * CVE-2018-1052: Fix the processing of partition keys containing multiple expressions
    * CVE-2018-1053: Ensure that all temporary files made with "pg_upgrade" are non-world-readable
    
    This PostgreSQL update also fixes over 60 bugs.
    
    https://www.postgresql.org/about/news/1829/

--- src/main/java/ru/yandex/qatools/embed/postgresql/distribution/Version.java
+++ src/main/java/ru/yandex/qatools/embed/postgresql/distribution/Version.java
@@ -6,9 +6,9 @@ import de.flapdoodle.embed.process.distribution.IVersion;
  * PostgreSQL Version enum
  */
 public enum Version implements IVersion {
-    V10_1("10.1-1"),
-    V9_6_6("9.6.6-1"),
-    @Deprecated V9_5_10("9.5.10-1"),;
+    V10_2("10.2-1"),
+    V9_6_7("9.6.7-1"),
+    @Deprecated V9_5_11("9.5.11-1"),;
 
     private final String specificVersion;
 
@@ -27,10 +27,10 @@ public enum Version implements IVersion {
     }
 
     public enum Main implements IVersion {
-        V9_5(V9_5_10),
-        V9_6(V9_6_6),
-        V10(V10_1),
-        PRODUCTION(V10_1);
+        V9_5(V9_5_11),
+        V9_6(V9_6_7),
+        V10(V10_2),
+        PRODUCTION(V10_2);
 
         private final IVersion _latest;
 
